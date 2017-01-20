ccd2c8a4e5f35e268c75113b56e4e3a5b17f8ef4
Tim Paisley <tepaisley@gmail.com>
2017-01-20 22:26:33 +1300

    Added ability to place blocks and different block types

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour {
+
+	void Start () {
+	
+	}
+	
+	void Update () {
+	
+	}
+}
---
+++ Assets/Scripts/PlaceBlock.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaceBlock : MonoBehaviour {
+
+	public GameObject placementBlock;
+
+	public GameObject sandBlock;
+	public GameObject dirtBlock;
+	public GameObject grassBlock;
+    
+	void Start () {
+	
+	}
+	
+	void Update () {
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast(ray, out hit, 100.0f)) {
+			if (hit.transform.tag == "Block") {
+				placementBlock.SetActive(true);
+				placementBlock.transform.position = hit.transform.position + hit.normal;
+			} else {
+				placementBlock.SetActive(false);
+			}
+		} else {
+			placementBlock.SetActive(false);
+		}
+
+		if (Input.GetMouseButtonDown(0)) {
+			if (placementBlock.activeSelf) {
+				Instantiate(sandBlock, placementBlock.transform.position, Quaternion.identity);
+			}
+		}
+	}
+}
