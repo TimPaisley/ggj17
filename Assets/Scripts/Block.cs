497e5be61f01a78cd7f1128a545e9463ff403b1e
Tim Paisley <tepaisley@gmail.com>
2017-01-21 12:13:31 +1300

    Initial (cleaner) commit

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -3,11 +3,26 @@ using System.Collections;
 
 public class Block : MonoBehaviour {
 
+	private GameManager gm;
+	private Renderer rend;
+
+	public enum BlockType { Sand, Dirt, Rock };
+	public BlockType type;
+
 	void Start () {
-	
+		gm = FindObjectOfType<GameManager>();
+		rend = GetComponent<Renderer>();
+
+		if (type == BlockType.Sand) {
+			rend.material = gm.sand;
+		} else if (type == BlockType.Dirt) {
+			rend.material = gm.dirt;
+		} else if (type == BlockType.Rock) {
+			rend.material = gm.rock;
+		}
 	}
-	
-	void Update () {
-	
+
+	void Update() {
+
 	}
 }
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GameManager : MonoBehaviour {
+
+	public GameObject marker;
+
+	public Material sand;
+	public Material dirt;
+	public Material rock;
+
+	private Stack<Block> inventory;
+	private Block focus;
+	
+	void Start () {
+		inventory = new Stack<Block>();
+	}
+	
+	void Update () {
+		Vector3 normal = RaycastForBlock();
+
+		// PLACE BLOCK: left mouse button
+		if (Input.GetMouseButtonDown(0) && focus != null && inventory.Count > 0) {
+			Block block = inventory.Pop();
+			block.transform.position = focus.transform.position + normal;
+			block.gameObject.SetActive(true);
+		}
+
+		// PICK UP BLOCK: right mouse button
+		else if (Input.GetMouseButtonDown(1) && focus != null && inventory.Count < 3) {
+			inventory.Push(focus);
+			focus.gameObject.SetActive(false);
+		}
+
+		// middle mouse button
+		else if (Input.GetMouseButtonDown(2)) {
+			// rotate camera
+		}
+	}
+
+	private Vector3 RaycastForBlock() {
+		RaycastHit hit;
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+		if (Physics.Raycast(ray, out hit, 100.0f)) {
+			Block block = hit.transform.GetComponent<Block>();
+
+			if (block != null) {
+				marker.transform.position = block.transform.position + (hit.normal * 0.52f);
+				marker.transform.rotation = Quaternion.LookRotation(hit.normal);
+
+				focus = block;
+				return hit.normal;
+			} else {
+				marker.transform.position = new Vector3(0.0f, 1000.0f, 0.0f);
+			}
+		}
+
+		focus = null;
+		return Vector3.zero;
+	}
+}
