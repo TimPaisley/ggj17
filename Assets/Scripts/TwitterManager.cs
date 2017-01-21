6e10afa04a6753ce01027b365d202bd1813e06ad
Jono Mingard <reason.koan@gmail.com>
2017-01-21 23:16:16 +1300

    Spawn bottles on tweet received

--- Assets/Scripts/TwitterManager.cs
+++ Assets/Scripts/TwitterManager.cs
@@ -5,15 +5,25 @@ using System.Threading;
 using UnityEngine;
 using System.Runtime.Serialization.Formatters.Binary;
 using System.IO;
+using System.Collections;
 
 public class TwitterManager : MonoBehaviour
 {
+	public delegate void TweetEventHandler(Tweet tweet);
+	public event TweetEventHandler OnTweet;
+
 	public TwitterAccess access { get; private set; }
+
+	Coroutine poller;
 
     void Awake() {
         access = new TwitterAccess();
         DontDestroyOnLoad(gameObject);
     }
+
+	void OnDestroy() {
+		Disconnect();
+	}
 
 	public void OpenAuthUrl() {
 		access.GetOAuthURL(url => System.Diagnostics.Process.Start(url));
@@ -46,70 +56,38 @@ public class TwitterManager : MonoBehaviour
 	public void DestroySavedCredentials() {
 		if (File.Exists(credentialsFile())) {
 			File.Delete(credentialsFile());
-			access.Disconnect();
+			Disconnect();
 		}
+	}
+
+	public void StartListening() {
+		poller = StartCoroutine(pollForTweets());
+	}
+
+	public void Disconnect() {
+		StopCoroutine(poller);
+		access.Disconnect();
 	}
 
 	string credentialsFile() {
 		return Application.persistentDataPath + "/credentials.gd";
 	}
-	
-	//public void Main ( string[] args )
-	//{
-	//	// Create twitteraccess object
-	//	access = new TwitterAccess();
-		
-	//	// Request authorisation key
-	//	access.GetOAuthURL( OnReceiveAuthURL );
-	
-	//	// Get all tweets with a location tag
-	//	QueryLocations world = new QueryLocations( new Coordinates( -180f, -90f ), new Coordinates( 180f, 90f ) );
-	//	access.AddQueryParameter( world );
-		
-	//	// and with the word "bieber" in them
-	//	access.AddQueryParameter( new QueryTrack( "bieber" ) );
-	//}
-	
-	//static void OnReceiveAuthURL( string url )
-	//{
-	//	// redirect usert to given URL to authorise this app
-	//	System.Diagnostics.Process.Start ( url );
-		
-	//	Debug.Log ( "Please enter passcode from authorisation URL: " );
-		
-	//	string passcode = "";
-	//	int testParse = 0;
-	//	passcode = Console.ReadLine();
-		
-	//	while ( !int.TryParse ( passcode, out testParse ) )
-	//	{
-	//		Debug.Log ( "Sorry, invalid passcode, please re-enter: " );
-	//		passcode = Console.ReadLine();
-	//	}
-		
-	//	// use given passcode to get signing pair for all requests
-	//	access.GetUserTokens ( passcode );
-		
-	//	while ( !access.IsOAuthed () )
-	//	{
-	//		Thread.Sleep( 500 );
-	//		Console.Write ( "." );
-	//	}
-		
-	//	Debug.Log ( "Authorised...connecting..." );
-	//	// Is authorised, can now connect
-	//	access.Connect ( false );
-		
-	//	while ( true )
-	//	{
-	//		if ( access.tweets.Count > 0 )
-	//		{
-	//			Tweet newTweet = access.tweets.Dequeue();
-	//			Debug.Log( newTweet.userName  + ": " + newTweet.status );
-	//		}
-	//	}
-		
-	//	// SHOULD ALWAYS CALL DISCONNECT on access object. THIS DOESN"T!!!
-	//}
-}
 
+	IEnumerator pollForTweets() {
+		access.AddQueryParameter(new Twitter.QueryTrack(access.screenName));
+		access.Connect(false);
+		Debug.Log("Listening for tweets to @" + access.screenName);
+
+		while (true) {
+			if (access.tweets.Count > 0) {
+				Twitter.Tweet newTweet = access.tweets.Dequeue();
+				if (newTweet.status.Contains("@" + access.screenName) && OnTweet != null) {
+					Debug.Log("TWEET!");
+					OnTweet(newTweet);
+				}
+			}
+
+			yield return new WaitForEndOfFrame();
+		}
+	}
+}
